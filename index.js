592ef8d1698b8416ec5c14468bcb979029c4a834
IMKINGJS <52960320+IMKINGJS@users.noreply.github.com>
2022-12-01 14:57:49 +0100

    Added coding file from vs code

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+// Find All Numbers Disappeared in an Array
+//Given an array nums of n integers where nums[i] is in the range [1, n], 
+// return an array of all the integers in the range [1, n] that do not appear in nums.
+
+//Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+
+var findDisappearedNumbers = function(nums) {
+    
+    let output = []
+    let  set = new Set(nums)
+     // return newArray ne
+      for(let i = 0; i < nums.length; i++){
+         if(!set.has(i+1)){output.push(i+1)}
+      }
+      return output;
+};
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1])) // output: [5,6]
